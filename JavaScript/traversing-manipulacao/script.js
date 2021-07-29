a027cce3c55d1c8c17c008ec2836fa8e9a046c57
Rafael Mascarenhas <rafaelmascarenhas63@gmail.com>
2021-07-29 18:55:11 -0300

    Inserindo efeitos no DOM

--- JavaScript/traversing-manipulacao/script.js
+++ JavaScript/traversing-manipulacao/script.js
@@ -1,81 +1,81 @@
-// //OUTERHTML, INNERHTML E INNERTEXT/
-// //Propriedades que retornam uma string contendo o html ou texto. É possível atribuir um novo valor para as mesmas element.innerText = 'Novo Texto'.
+//OUTERHTML, INNERHTML E INNERTEXT/
+//Propriedades que retornam uma string contendo o html ou texto. É possível atribuir um novo valor para as mesmas element.innerText = 'Novo Texto'.
 
-// const menu = document.querySelector('.menu');
+const menu = document.querySelector('.menu');
 
-// menu.outerHTML; // todo o html do elemento
-// menu.innerHTML; // html interno
-// menu.innerText; // texto, sem tags
+menu.outerHTML; // todo o html do elemento
+menu.innerHTML; // html interno
+menu.innerText; // texto, sem tags
 
-// menu.innerText = '<p>Texto</p>'; // a tag vai como texto
-// menu.innerHTML = '<p>Texto</p>'; // a tag é renderizada
-
-
-// //TRASVERSING
-// //Como navegar pelo DOM, utilizando suas propriedades e métodos.
-
-// const lista = document.querySelector('.animais-lista');
-
-// lista.parentElement; // pai
-// lista.parentElement.parentElement; // pai do pai
-// lista.previousElementSibling; // elemento acima
-// lista.nextElementSibling; // elemento abaixo
-
-// lista.children; // HTMLCollection com os filhos
-// lista.children[0]; // primeiro filho
-// lista.children[--lista.children.length]; // último filho
-
-// lista.querySelectorAll('li'); // todas as LI's
-// lista.querySelector('li:last-child'); // último filho
+menu.innerText = '<p>Texto</p>'; // a tag vai como texto
+menu.innerHTML = '<p>Texto</p>'; // a tag é renderizada
 
 
-// //ELEMENT VS NODE
-// //Element's represetam um elemento html, ou seja, uma tag. Node representa um nó, e pode ser um elemento (Element), texto, comentário, quebra de linha e mais.
+//TRASVERSING
+//Como navegar pelo DOM, utilizando suas propriedades e métodos.
 
-// const lista = document.querySelector('.animais-lista');
+const lista = document.querySelector('.animais-lista');
 
-// lista.previousElementSibling; // elemento acima
-// lista.previousSibling; // node acima
+lista.parentElement; // pai
+lista.parentElement.parentElement; // pai do pai
+lista.previousElementSibling; // elemento acima
+lista.nextElementSibling; // elemento abaixo
 
-// lista.firstChild; // primeiro node child
-// lista.childNodes; // todos os node child
+lista.children; // HTMLCollection com os filhos
+lista.children[0]; // primeiro filho
+lista.children[--lista.children.length]; // último filho
 
-// //MANIPULANDO ELEMENTOS
-// //É possível mover elementos no dom com métodos de Node.
+lista.querySelectorAll('li'); // todas as LI's
+lista.querySelector('li:last-child'); // último filho
 
-// const lista = document.querySelector('.animais-lista');
-// const contato = document.querySelector('.contato');
-// const titulo = contato.querySelector('.titulo');
 
-// contato.appendChild(lista); // move lista para o final de contato
-// contato.insertBefore(lista, titulo); // insere a lista antes de titulo
-// contato.removeChild(titulo); // remove titulo de contato
-// contato.replaceChild(lista, titulo); // substitui titulo por lista
+//ELEMENT VS NODE
+//Element's represetam um elemento html, ou seja, uma tag. Node representa um nó, e pode ser um elemento (Element), texto, comentário, quebra de linha e mais.
 
-// //NOVOS ELEMENTOS
-// //Podemos criar novos elementos com o método createElement()
+const lista = document.querySelector('.animais-lista');
 
-// const animais = document.querySelector('.animais');
+lista.previousElementSibling; // elemento acima
+lista.previousSibling; // node acima
 
-// const novoH1 = document.createElement('h1');
-// novoH1.innerText = 'Novo Título';
-// novoH1.classList.add('titulo');
+lista.firstChild; // primeiro node child
+lista.childNodes; // todos os node child
 
-// animais.appendChild(novoH1);
+//MANIPULANDO ELEMENTOS
+//É possível mover elementos no dom com métodos de Node.
 
-// //CLONAR ELEMENTOS
-// //Todo elemento selecionado é único. Para criarmos um novo elemento baseado no anterior, é necessário utilizar o método cloneNode()
+const lista = document.querySelector('.animais-lista');
+const contato = document.querySelector('.contato');
+const titulo = contato.querySelector('.titulo');
 
-// const titulo = document.querySelector('h1');
-// const titulo2 = document.querySelector('h1');
-// const novoTitulo = titulo;
-// // titulo, titulo2 e novoTitulo são iguais
+contato.appendChild(lista); // move lista para o final de contato
+contato.insertBefore(lista, titulo); // insere a lista antes de titulo
+contato.removeChild(titulo); // remove titulo de contato
+contato.replaceChild(lista, titulo); // substitui titulo por lista
 
-// const cloneTitulo = titulo.cloneNode(true);
-// const contato = document.querySelector('.contato');
-// contato.appendChild(cloneTitulo);
+//NOVOS ELEMENTOS
+//Podemos criar novos elementos com o método createElement()
 
-//true sinaliza para incluir os filhos
+const animais = document.querySelector('.animais');
+
+const novoH1 = document.createElement('h1');
+novoH1.innerText = 'Novo Título';
+novoH1.classList.add('titulo');
+
+animais.appendChild(novoH1);
+
+//CLONAR ELEMENTOS
+//Todo elemento selecionado é único. Para criarmos um novo elemento baseado no anterior, é necessário utilizar o método cloneNode()
+
+const titulo = document.querySelector('h1');
+const titulo2 = document.querySelector('h1');
+const novoTitulo = titulo;
+// titulo, titulo2 e novoTitulo são iguais
+
+const cloneTitulo = titulo.cloneNode(true);
+const contato = document.querySelector('.contato');
+contato.appendChild(cloneTitulo);
+
+true sinaliza para incluir os filhos
 
 //EXERCÍCIO
 
